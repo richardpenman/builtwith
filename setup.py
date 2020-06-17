1de4a34f92dd8ba9b5755c48bb4f0332a0ef1998
Richard Penman <richard.penman@gmail.com>
2020-06-17 11:51:41 -0400

    updated setup url

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ def read(filename):
 
 setup(
     name='builtwith', 
-    version='1.3.2',
+    version='1.3.4',
     packages=['builtwith'],
     package_dir={'builtwith' : '.'}, # look for package contents in current directory
     author='Richard Penman',
-    author_email='richard@webscraping.com',
+    author_email='richard.penman@gmail.com',
     description='Detect the technology used by a website, such as Apache, JQuery, and Wordpress.',
     long_description=read('README.rst'),
-    url='https://bitbucket.org/richardpenman/builtwith',
+    url='https://github.com/richardpenman/builtwith',
     license='lgpl',
     install_requires=['six'],
 )
