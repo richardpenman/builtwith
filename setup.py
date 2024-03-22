6f59245b4a48c83bde4273961d526015398e8f72
Richard Penman <{ID}+{username}@users.noreply.github.com>
2024-03-22 14:55:20 +0800

    pass flake8 tests

--- setup.py
+++ setup.py
@@ -1,18 +1,22 @@
 import os
 from distutils.core import setup
 
+
 def read(filename):
     return open(os.path.join(os.path.dirname(__file__), filename)).read()
 
+
 setup(
-    name='builtwith', 
-    version='1.4.0',
-    packages=['builtwith'],
-    package_dir={'builtwith' : 'src'}, # look for package contents in current directory
-    author='Richard Penman',
-    author_email='richard.penman@gmail.com',
-    description='Detect the technology used by a website, such as Apache, JQuery, and Wordpress.',
-    long_description=read('README.rst'),
-    url='https://github.com/richardpenman/builtwith',
-    license='lgpl',
+    name="builtwith",
+    version="1.4.0",
+    packages=["builtwith"],
+    package_dir={"builtwith": "src"},  # look for package contents in current directory
+    data_files=[("builtwith", ["src/apps.json"])],
+    author="Richard Penman",
+    author_email="richard.penman@gmail.com",
+    description="Detect the technology used by a website\
+                , such as Apache, JQuery, and Wordpress.",
+    long_description=read("README.rst"),
+    url="https://github.com/richardpenman/builtwith",
+    license="lgpl",
 )
--- src/__init__.py
+++ src/__init__.py
@@ -6,19 +6,7 @@ import urllib.request
 
 
 def builtwith(url, headers=None, html=None, user_agent="builtwith"):
-    """Detect the technology used to build a website
-
-    >>> builtwith('http://wordpress.com')
-    {u'blogs': [u'PHP', u'WordPress'], u'font-scripts': [u'Google Font API'], u'web-servers': [u'Nginx'], u'javascript-frameworks': [u'Modernizr'], u'programming-languages': [u'PHP'], u'cms': [u'WordPress']}
-    >>> builtwith('http://webscraping.com')
-    {u'javascript-frameworks': [u'jQuery', u'Modernizr'], u'web-frameworks': [u'Twitter Bootstrap'], u'web-servers': [u'Nginx']}
-    >>> builtwith('http://microsoft.com')
-    {u'javascript-frameworks': [u'jQuery'], u'mobile-frameworks': [u'jQuery Mobile'], u'operating-systems': [u'Windows Server'], u'web-servers': [u'IIS']}
-    >>> builtwith('http://jquery.com')
-    {u'cdn': [u'CloudFlare'], u'web-servers': [u'Nginx'], u'javascript-frameworks': [u'jQuery', u'Modernizr'], u'programming-languages': [u'PHP'], u'cms': [u'WordPress'], u'blogs': [u'PHP', u'WordPress']}
-    >>> builtwith('http://joomla.org')
-    {u'font-scripts': [u'Google Font API'], u'miscellaneous': [u'Gravatar'], u'web-servers': [u'LiteSpeed'], u'javascript-frameworks': [u'jQuery'], u'programming-languages': [u'PHP'], u'web-frameworks': [u'Twitter Bootstrap'], u'cms': [u'Joomla'], u'video-players': [u'YouTube']}
-    """
+    """Detect the technology used to build a website"""
     techs = {}
 
     # check URL
@@ -65,12 +53,11 @@ def builtwith(url, headers=None, html=None, user_agent="builtwith"):
         # XXX add proper meta data parsing
         if isinstance(html, bytes):
             html = html.decode()
-        metas = dict(
-            re.compile(
-                "<meta[^>]*?name=['\"]([^>]*?)['\"][^>]*?content=['\"]([^>]*?)['\"][^>]*?>",
-                re.IGNORECASE,
-            ).findall(html)
+        regex = re.compile(
+            "<meta[^>]*?name=['\"]([^>]*?)['\"][^>]*?content=['\"]([^>]*?)['\"][^>]*?>",
+            re.IGNORECASE
         )
+        metas = dict(regex.findall(html))
         for app_name, app_spec in data["apps"].items():
             for name, content in app_spec.get("meta", {}).items():
                 if name in metas:
@@ -124,7 +111,7 @@ def contains_dict(d1, d2):
     return True
 
 
-def load_apps(filename="apps.json.py"):
+def load_apps(filename="apps.json"):
     """Load apps from Wappalyzer JSON (https://github.com/ElbertF/Wappalyzer)"""
     # get the path of this filename relative to the current script
     # XXX add support to download update
